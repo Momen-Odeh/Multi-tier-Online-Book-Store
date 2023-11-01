ea39173f4c0636de55c2fd6ea64efe141f63f2dd
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-11-01 15:30:44 +0200

    book by subject API connect frontend server to catalog server

--- Frontend Server/APIs/search.py
+++ Frontend Server/APIs/search.py
@@ -3,5 +3,5 @@ import requests
 
 @app.route('/search/<string:topic>', methods=['GET'])
 def query_books_by_subject(topic):
-    search_response = requests.get(f'http://127.0.0.1:5001/search/{topic}')
+    search_response = requests.get(f'http://127.0.0.1:5000/search/{topic}')
     return search_response.text,search_response.status_code
