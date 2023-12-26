cefed8881469dcb44cb35970cb18684dc5925466
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-12-27 01:27:27 +0200

    update datebase file

--- Frontend Server/APIs/updateBook.py
+++
@@ -1,8 +0,0 @@
-from FlaskSetup import app
-from flask import request
-import requests
-
-@app.route('/books/<int:id>', methods=['PUT'])
-def updateBook(id):
-    purchase_response = requests.put(f'http://localhost:5001/books/{id}',json=request.get_json())
-    return purchase_response.text,purchase_response.status_code
--- Frontend Server/main.py
+++ Frontend Server/main.py
@@ -3,7 +3,6 @@ from FlaskSetup import app
 from APIs.info import query_book_by_item
 from APIs.search import query_books_by_subject
 from APIs.purchase import purchase_item
-from APIs.updateBook import updateBook
 CORS(app)
 
 if __name__ == '__main__':
