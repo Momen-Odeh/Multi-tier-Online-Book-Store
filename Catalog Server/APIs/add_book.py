2d8e2a6aca36ce8b3d4a32c49f35937061e8300a
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-12-27 01:22:09 +0200

    add api for inseart new book

---
+++ Catalog Server/APIs/add_book.py
@@ -0,0 +1,34 @@
+from FlaskSetup import app
+from flask import request,jsonify
+from SQLiteConnection import get_db_connection
+import requests
+urlReplicaServer = "http://localhost:5003"
+@app.route('/books', methods=['POST'])
+def addBook():
+    try:
+        data = request.json
+        topic = data.get('topic')
+        title = data.get('title')
+        price = data.get('price')
+        quantity = data.get('quantity')
+
+        if(topic and price and title and quantity):
+            sql_query = 'INSERT INTO Books (topic, title, price, quantity) VALUES (?, ?, ?, ?)'
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute(sql_query,(topic, title, price, quantity))
+            conn.commit()
+            # get topic from DB
+            cursor.execute(f"select id from Books where topic = '{topic}' and title = '{title}' and price ='{price}' and quantity = '{quantity}' ;")
+            id = cursor.fetchone()[0]
+            #
+            conn.close()
+            return jsonify({
+            "status": "insert successfully",
+            "id": id
+            }), 201
+        return "please enter all fields", 400
+
+    except Exception as e:
+        print(f"Error processing request: {str(e)}")
+        return "Bad Request", 400
