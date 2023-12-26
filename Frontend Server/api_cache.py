d3e66ec8b8d79e2401248974b40aa2b4e237b351
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-27 00:15:43 +0200

    add update book api

--- Frontend Server/api_cache.py
+++ Frontend Server/api_cache.py
@@ -3,16 +3,16 @@ api_cache = {}
 
 
 def get_from_cache(key):
-    data = api_cache.get(key, None)
+    data = api_cache.get(key.strip().lower(), None)
     print("get data: ", data)
     return data
 
 
 def add_data_to_cache(key, value):
-    api_cache[key] = value
+    api_cache[key.strip().lower()] = value
     print("add data: ", value)
 
 
 def delete_data_from_cache(key):
-    api_cache.pop(key, None)
+    api_cache.pop(key.strip().lower(), None)
     print("delete data: ", key)
