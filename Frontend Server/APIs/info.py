d7a39f8400f5c72de747f539e0526ab6e4bd55c9
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-26 21:44:49 +0200

    use cache mechanism in apis

--- Frontend Server/APIs/info.py
+++ Frontend Server/APIs/info.py
@@ -1,7 +1,13 @@
 from FlaskSetup import app
 import requests
+from api_cache import get_from_cache, add_data_to_cache
+
 
 @app.route('/info/<int:item_id>', methods=['GET'])
 def query_book_by_item(item_id):
-    info_response = requests.get(f'http://catalog:5001/info/{item_id}')
-    return info_response.text,info_response.status_code
+    data = get_from_cache(str(item_id))
+    if data:
+        return data, 200
+    info_response = requests.get(f'localhost:5001/info/{item_id}')
+    add_data_to_cache(str(item_id), info_response.text)
+    return info_response.text, info_response.status_code
--- Frontend Server/APIs/purchase.py
+++ Frontend Server/APIs/purchase.py
@@ -1,7 +1,11 @@
 from FlaskSetup import app
 import requests
+from api_cache import delete_data_from_cache
+
 
 @app.route('/purchase/<int:item_id>', methods=['POST'])
 def purchase_item(item_id):
-    purchase_response = requests.post(f'http://order:5002/purchase/{item_id}')
-    return purchase_response.text,purchase_response.status_code
+    purchase_response = requests.post(f'localhost:5002/purchase/{item_id}')
+    if purchase_response.status_code == 200:
+        delete_data_from_cache(str(item_id))
+    return purchase_response.text, purchase_response.status_code
