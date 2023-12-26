8f3a1ebd1a84f83da406f3ac022b7798706dd61b
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-27 00:49:41 +0200

    update status code in add book

--- Frontend Server/APIs/add_book.py
+++ Frontend Server/APIs/add_book.py
@@ -9,6 +9,6 @@ from load_balancing_catalog import round_robin
 def add_book():
     data = request.get_json()
     info_response = requests.post(f'{round_robin()}/books', json=data)
-    if info_response.status_code == 200:
+    if info_response.status_code == 201:
         delete_data_from_cache(str(data['topic']).strip().lower())
     return info_response.text, info_response.status_code
